4acfd81e420d0f56556d755d6927b2b71751886f
Bumbi <annika.borgesson@gmail.com>
2023-10-18 10:45:39 +0200

    Make editbtn work, add save,del btns

--- assets/script/script.js
+++ assets/script/script.js
@@ -53,6 +53,19 @@ function activateDeleteListeners() {
   });
 }
 
+/*-- FUNCTION FOR EDIT BUTTONS--*/
+function activateEditListeners() {
+  const editBtn = document.querySelectorAll(".editBtn");
+  const updateController = document.querySelectorAll(".update-controller");
+  const inputs = document.querySelectorAll(".input-controller textarea");
+  editBtn.forEach((eb, i) => {
+    eb.addEventListener("click", () => {
+      updateController[i].style.display = "block";
+      inputs[i].disabled = false;
+    });
+  });
+}
+
 function deleteItem(i) {
   itemsArray.splice(i, 1);
   localStorage.setItem("items", JSON.stringify(itemsArray));
